7ca6fae6f0224c9b9eade0a8b5228877467f3423
jasonazuela2001-arch <jasonazuela2001@gmail.com>
2026-09-23 15:53:12 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@react-plugin/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // 👈 Make sure this matches your repository name exactly
+  base: '/portfolio/', // 👈 Must match your GitHub repository name exactly
 })
